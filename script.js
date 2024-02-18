ad4f2cba0e958c0902f3ab47fa43fac5d5e9203c
CalebFrazier0008 <157289715+CalebFrazier0008@users.noreply.github.com>
2024-02-18 07:01:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function showHide1() {
+  const questionNode = document.querySelector(".QuestionCon1");
+  const answerNode = questionNode.children[1];
+  const headerNode = document.querySelector(".Question-1");
+  const plusNode = headerNode.children[0];
+  const minusNode = headerNode.children[1];
+  answerNode.classList.toggle("Hidden");
+  plusNode.classList.toggle("Hidden");
+  minusNode.classList.toggle("Hidden");
+}
+function showHide2() {
+  const questionNode = document.querySelector(".QuestionCon2");
+  const answerNode = questionNode.children[1];
+  const headerNode = document.querySelector(".Question-2");
+  const plusNode = headerNode.children[0];
+  const minusNode = headerNode.children[1];
+  answerNode.classList.toggle("Hidden");
+  plusNode.classList.toggle("Hidden");
+  minusNode.classList.toggle("Hidden");
+}
+function showHide3() {
+  const questionNode = document.querySelector(".QuestionCon3");
+  const answerNode = questionNode.children[1];
+  const headerNode = document.querySelector(".Question-3");
+  const plusNode = headerNode.children[0];
+  const minusNode = headerNode.children[1];
+  answerNode.classList.toggle("Hidden");
+  plusNode.classList.toggle("Hidden");
+  minusNode.classList.toggle("Hidden");
+}
+function showHide4() {
+  const questionNode = document.querySelector(".QuestionCon4");
+  const answerNode = questionNode.children[1];
+  const headerNode = document.querySelector(".Question-4");
+  const plusNode = headerNode.children[0];
+  const minusNode = headerNode.children[1];
+  answerNode.classList.toggle("Hidden");
+  plusNode.classList.toggle("Hidden");
+  minusNode.classList.toggle("Hidden");
+}
